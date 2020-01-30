b92116d21ca2eee06099fef0a8561298c5831703
Tom Millichamp <tom@millichamps.com>
2020-01-30 12:03:35 +0000

    Add project files.

---
+++ Nunit Test Runner/Form1.Designer.cs
@@ -0,0 +1,221 @@
+﻿namespace Nunit_Test_Runner
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBoxTestNames = new System.Windows.Forms.ListBox();
+            this.nunitdllpath = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBoxRunnerPath = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBoxResults = new System.Windows.Forms.TextBox();
+            this.textBoxCMD = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // listBoxTestNames
+            // 
+            this.listBoxTestNames.FormattingEnabled = true;
+            this.listBoxTestNames.Location = new System.Drawing.Point(188, 76);
+            this.listBoxTestNames.Name = "listBoxTestNames";
+            this.listBoxTestNames.ScrollAlwaysVisible = true;
+            this.listBoxTestNames.Size = new System.Drawing.Size(579, 160);
+            this.listBoxTestNames.TabIndex = 0;
+            // 
+            // nunitdllpath
+            // 
+            this.nunitdllpath.Location = new System.Drawing.Point(188, 36);
+            this.nunitdllpath.Name = "nunitdllpath";
+            this.nunitdllpath.Size = new System.Drawing.Size(579, 20);
+            this.nunitdllpath.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(13, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "1";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(63, 33);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(108, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Open DLL...";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(26, 76);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(13, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "2";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(63, 71);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(108, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Retrieve Tests";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(26, 153);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(145, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "3        Select Test to Execute";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(26, 263);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(13, 13);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "4";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(45, 258);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(138, 23);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Select Console Runner...";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBoxRunnerPath
+            // 
+            this.textBoxRunnerPath.Location = new System.Drawing.Point(190, 260);
+            this.textBoxRunnerPath.Name = "textBoxRunnerPath";
+            this.textBoxRunnerPath.Size = new System.Drawing.Size(577, 20);
+            this.textBoxRunnerPath.TabIndex = 7;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(45, 321);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(138, 23);
+            this.button4.TabIndex = 8;
+            this.button4.Text = "Run Test";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(26, 326);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(13, 13);
+            this.label5.TabIndex = 2;
+            this.label5.Text = "5";
+            // 
+            // textBoxResults
+            // 
+            this.textBoxResults.Location = new System.Drawing.Point(190, 350);
+            this.textBoxResults.Multiline = true;
+            this.textBoxResults.Name = "textBoxResults";
+            this.textBoxResults.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBoxResults.Size = new System.Drawing.Size(577, 88);
+            this.textBoxResults.TabIndex = 9;
+            // 
+            // textBoxCMD
+            // 
+            this.textBoxCMD.Location = new System.Drawing.Point(190, 321);
+            this.textBoxCMD.Name = "textBoxCMD";
+            this.textBoxCMD.Size = new System.Drawing.Size(577, 20);
+            this.textBoxCMD.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBoxCMD);
+            this.Controls.Add(this.textBoxResults);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.textBoxRunnerPath);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.nunitdllpath);
+            this.Controls.Add(this.listBoxTestNames);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBoxTestNames;
+        private System.Windows.Forms.TextBox nunitdllpath;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBoxRunnerPath;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBoxResults;
+        private System.Windows.Forms.TextBox textBoxCMD;
+    }
+}
+
---
+++ Nunit Test Runner/Form1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Nunit_Test_Runner
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Properties.Settings.Default.dllpath = this.nunitdllpath.Text;
+            Properties.Settings.Default.runnerpath = this.textBoxRunnerPath.Text;
+            Properties.Settings.Default.Save();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            this.nunitdllpath.Text = Properties.Settings.Default.dllpath;
+            this.textBoxRunnerPath.Text = Properties.Settings.Default.runnerpath;
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (this.openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                this.nunitdllpath.Text = this.openFileDialog1.FileName;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.listBoxTestNames.Items.Clear();
+            Assembly assembly = Assembly.LoadFrom(this.nunitdllpath.Text);
+            foreach (Type type in assembly.GetTypes())
+            {
+                foreach (MethodInfo methodInfo in type.GetMethods())
+                {
+                    var attributes = methodInfo.GetCustomAttributes(true);
+                    foreach (var attr in attributes)
+                    {
+                        if (attr.ToString() == "NUnit.Framework.TestAttribute")
+                        {
+                            var methodName = methodInfo.Name;
+                            this.listBoxTestNames.Items.Add(methodName);
+                        }
+                    }
+                }
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (this.openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                this.textBoxRunnerPath.Text = this.openFileDialog1.FileName;
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            this.textBoxResults.Clear();
+            //string selectedTest = this.listBoxTestNames.Items[listBoxTestNames.SelectedIndex].ToString();
+            string selectedTest = this.listBoxTestNames.SelectedItem.ToString();
+            string DllPath = "\"" + this.nunitdllpath.Text + "\"";
+            string runnerPath = "\"" + this.textBoxRunnerPath.Text + "\"";
+            string args = "--test=" + selectedTest + " " + DllPath;
+            this.textBoxCMD.Text = runnerPath + " " + args;
+            ExecuteCommand(runnerPath, args);
+        }
+
+        public bool ExecuteCommand(string exeDir, string args)
+        {
+            try
+            {
+                ProcessStartInfo procStartInfo = new ProcessStartInfo();
+
+                procStartInfo.FileName = exeDir;
+                procStartInfo.Arguments = args;
+                procStartInfo.RedirectStandardOutput = true;
+                procStartInfo.UseShellExecute = false;
+                procStartInfo.CreateNoWindow = true;
+
+                using (Process process = new Process())
+                {
+                    process.StartInfo = procStartInfo;
+                    process.Start();
+
+                    process.WaitForExit();
+
+                    string result = process.StandardOutput.ReadToEnd();
+                    Console.WriteLine(result);
+                    this.textBoxResults.AppendText(result);
+                }
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("*** Error occured executing the following commands.");
+                this.textBoxResults.AppendText("**** Error occured ****");
+                Console.WriteLine(exeDir);
+                Console.WriteLine(args);
+                Console.WriteLine(ex.Message);
+                this.textBoxResults.AppendText(ex.Message);
+                return false;
+            }
+        }
+    }
+}
